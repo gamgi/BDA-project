f923727460f09491ef0dbea05b9d79c030c0368d
Pietu <pietu.roisko@aalto.fi>
2018-11-15 22:50:30 +0200

    Correctly join on municipality and year, minor changes

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
-from data import Data
-import config
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
+from data import Data
+import config
 
 
 df_income, df_results = Data.load()
-#print(df_results.head())
-#print(df_income.head())
+print("resultss shape:", df_results.shape)
+print(df_results.sort_values(by="municipality_code").head())
+print("All income shape:", df_income.shape)
+print(df_income.sort_values(by="municipality_code").head())
 
-df_all = pd.merge(df_income, df_results, on="municipality_code")
+df_all = pd.merge(df_results, df_income, on=["municipality_code", "year"], how='inner')
 
-print(df_all.head())
+print("All records shape:", df_all.shape)
+print(df_all.sort_values(by=["municipality_code", "year"]).head(n=20))
 
-sample = df_all.sample(frac=0.1)
+sample = df_all.sample(frac=1)
 
 # Plot
-x = sample.loc[: , "taxable income, median"].values
-y = sample.loc[: , "mean"].values
-fig, ax = plt.subplots(figsize=(10,5))
-ax.scatter(x, y, 10, marker=',')
-ax.set_xlabel('median income')
-ax.set_ylabel('exam mean')
+fig, ax = plt.subplots(figsize=(10, 5))
+years = np.unique(df_all[['year']].values.flatten())
+for i, year in enumerate(years):
+    df = sample.loc[sample['year'] == year]
+    x = df.loc[:, "taxable income, median"].values
+    y = df.loc[:, "mean"].values
+    ax.scatter(x, y, 10, marker=',', label=year)
+ax.set_xlabel('median income for municipality')
+ax.set_ylabel('exam mean for school')
+plt.legend()
 plt.show()
