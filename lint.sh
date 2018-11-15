dba7b7271e1b976f69041010f9bad8937dc2a55e
Pietu <pietu.roisko@aalto.fi>
2018-11-15 22:15:54 +0200

    Ignore E402

--- lint.sh
+++ lint.sh
@@ -1,1 +1,4 @@
-./venv/bin/autopep8 --in-place --aggressive --aggressive --exclude venv --recursive --max-line-length 90 .
+# Aggressive in place linting
+# max-line leght 90
+# ignore E402 imports at beginning of file due to Tkinter config required before some imports
+./venv/bin/autopep8 --in-place --aggressive --aggressive --exclude venv --recursive --max-line-length 90 --ignore=E402 .
