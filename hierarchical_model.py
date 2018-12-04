29096c970bcc9ebf6750bf34b9c2d06485c8f355
Pietu <pietu.roisko@aalto.fi>
2018-12-04 17:07:11 +0200

    Add base for hierarchical model

--- hierarchical_model.py
+++ hierarchical_model.py
@@ -21,55 +21,30 @@ df_all = pd.merge(df_results, df_income, on=["municipality_code", "year"], how='
 print("Using a sample for calculations")
 excluded_munis = ['Helsinki', 'Espoo', 'Kauniainen']
 df = df_all[(~df_all['municipality'].isin(excluded_munis)) & (df_all['season'] == 'K')]
-# df = df_all[(df_all['season'] == 'K')]
 sample = df.sample(frac=0.2, random_state=1)
 
 # Prepare data
 N, d = sample.shape
 y = sample['mean']
 x = sample['taxable income, median']
+z = sample['municipality_code']
 
 data = dict(
     N=N,
     x=x,
     y=y,
+    z=z,
     sigma0=4.0
 )
 
 
 def plot_model(data, samples):
-    x = data['x']
-    y = data['y']
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 10))
-    ax1.scatter(x, y, 5)
-
-    ax1.set_xlabel("median income")
-    ax1.set_ylabel("mean exam score")
-    ax1.set_xlim((15000, 35000))
-    # Error range
-    for p in np.arange(5, 95, 5):
-        ax1.plot(
-            x,
-            np.percentile(samples["mu"], p, axis=0),
-            alpha=0.1,
-            linewidth=10,
-            color='gray'
-        )
-    # Mean
-    ax1.plot(
-        x,
-        np.percentile(samples["mu"], 50, axis=0),
-    )
-    ax2.hist(samples["beta"], 50)
-    ax2.set_xlabel("beta")
-    ax3.hist(samples["alpha"], 50)
-    ax3.set_xlabel("alpha")
-    plt.show()
+    pass
 
 
 # Prepare stan
 print("Compiling model...")
-model = Model('model.linear.stan')
+model = Model('model.hierarchical.stan')
 print("Fitting data to model...")
 fit = model.sample(data=data)
 samples = fit.extract(permuted=True)
--- model_utils.py
+++ model_utils.py
@@ -15,33 +15,3 @@ class Model:
     def sample(self, data, **kwargs):
         self.fit = self.stan_model.sampling(data=data, seed=self.seed, **kwargs)
         return self.fit
-
-
-def plot_model(data, samples):
-    x = data['x']
-    y = data['y']
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 10))
-    ax1.scatter(x, y, 5)
-
-    ax1.set_xlabel("median income")
-    ax1.set_ylabel("mean exam score")
-    ax1.set_xlim((15000, 35000))
-    # Error range
-    for p in np.arange(5, 95, 5):
-        ax1.plot(
-            x,
-            np.percentile(samples["mu"], p, axis=0),
-            alpha=0.1,
-            linewidth=10,
-            color='gray'
-        )
-    # Mean
-    ax1.plot(
-        x,
-        np.percentile(samples["mu"], 50, axis=0),
-    )
-    ax2.hist(samples["beta"], 50)
-    ax2.set_xlabel("beta")
-    ax3.hist(samples["alpha"], 50)
-    ax3.set_xlabel("alpha")
-    plt.show()
